4118307bbb09360cabe8b21a29e55a542b53a381
inpakta2023 <158587270+inpakta2023@users.noreply.github.com>
2024-02-14 14:40:58 +0200

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,173 @@
+/* Theme Scripts */
+
+(function($){ "use strict";
+             
+    $(window).on('load', function() {
+        $('body').addClass('loaded');
+    });
+             
+/*=========================================================================
+	Sticky Header
+=========================================================================*/
+    function headerHeight(){
+        var height = $("#header").height();
+        $('.header-height').css('height', height+'px');
+    }
+
+	$(function() {
+		var header = $("#header"),
+			yOffset = 0,
+			triggerPoint = 80;
+        headerHeight();
+        $(window).resize(headerHeight);
+		$(window).on( 'scroll', function() {
+			yOffset = $(window).scrollTop();
+
+			if (yOffset >= triggerPoint) {
+				header.addClass("navbar-fixed-top animated slideInDown");
+			} else {
+				header.removeClass("navbar-fixed-top animated slideInDown");
+			}
+
+		});
+	});
+
+/*=========================================================================
+    Mobile Menu
+=========================================================================*/  
+    $('.menu-wrap ul.nav').slicknav({
+        prependTo: '.header-section .navbar',
+        label: '',
+        allowParentLinks: true
+    });
+
+/*=========================================================================
+    Button Style
+=========================================================================*/               
+    $('.default-btn').on('mouseenter', function (e) {
+        var parentOffset = $(this).offset(),
+                relX = e.pageX - parentOffset.left,
+                relY = e.pageY - parentOffset.top;
+        $(this).find('span').css({top: relY, left: relX})
+    }).on('mouseout', function (e) {
+        var parentOffset = $(this).offset(),
+                relX = e.pageX - parentOffset.left,
+                relY = e.pageY - parentOffset.top;
+        $(this).find('span').css({top: relY, left: relX})
+    });    
+
+/*=========================================================================
+    Testimonial Carousel
+=========================================================================*/
+    $('#testimonial-carousel').owlCarousel({
+        loop: true,
+        margin: 10,
+        center: false,
+        autoplay: true,
+        smartSpeed: 500,
+        nav: false,
+        navText: ['<i class="fa fa-caret-left"></i>', '<i class="fa fa-caret-right"></i>'],
+        dots: true,
+        responsive : {
+            0 : {
+                items: 1
+            },
+            480 : {
+                items: 1,
+            },
+            768 : {
+                items: 1,
+            },
+            992 : {
+                items: 2,
+            }
+        }
+    });
+
+/*=========================================================================
+    Sponsor Carousel
+=========================================================================*/
+    $('#sponsor-carousel').owlCarousel({
+        loop: true,
+        margin: 5,
+        center: false,
+        autoplay: true,
+        smartSpeed: 500,
+        nav: false,
+        navText: ['<i class="fa fa-caret-left"></i>', '<i class="fa fa-caret-right"></i>'],
+        dots: false,
+        responsive : {
+            0 : {
+                items: 2
+            },
+            480 : {
+                items: 3,
+            },
+            768 : {
+                items: 3,
+            },
+            992 : {
+                items: 6,
+            }
+        }
+    });
+
+/*=========================================================================
+	Initialize smoothscroll plugin
+=========================================================================*/
+	smoothScroll.init({
+		offset: 60
+	});
+	 
+/*=========================================================================
+	Scroll To Top
+=========================================================================*/ 
+    $(window).on( 'scroll', function () {
+        if ($(this).scrollTop() > 100) {
+            $('#scroll-to-top').fadeIn();
+        } else {
+            $('#scroll-to-top').fadeOut();
+        }
+    });
+
+/*=========================================================================
+	WOW Active
+=========================================================================*/ 
+   new WOW().init();
+    
+/*=========================================================================
+	MAILCHIMP
+=========================================================================*/ 
+
+    if ($('.subscribe_form').length>0) {
+        /*  MAILCHIMP  */
+        $('.subscribe_form').ajaxChimp({
+            language: 'es',
+            callback: mailchimpCallback,
+            url: "//alexatheme.us14.list-manage.com/subscribe/post?u=48e55a88ece7641124b31a029&amp;id=361ec5b369" 
+        });
+    }
+
+    function mailchimpCallback(resp) {
+        if (resp.result === 'success') {
+            $('#subscribe-result').addClass('subs-result');
+            $('.subscription-success').text(resp.msg).fadeIn();
+            $('.subscription-error').fadeOut();
+
+        } else if(resp.result === 'error') {
+            $('#subscribe-result').addClass('subs-result');
+            $('.subscription-error').text(resp.msg).fadeIn();
+        }
+    }
+    $.ajaxChimp.translations.es = {
+        'submit': 'Submitting...',
+        0: 'We have sent you a confirmation email',
+        1: 'Please enter your email',
+        2: 'An email address must contain a single @',
+        3: 'The domain portion of the email address is invalid (the portion after the @: )',
+        4: 'The username portion of the email address is invalid (the portion before the @: )',
+        5: 'This email address looks fake or invalid. Please enter a real email address'
+    };
+    
+
+})(jQuery);
